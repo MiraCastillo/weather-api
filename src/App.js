25e23bc7828b2ead7797d8e79eebd3568a5d2c66
Mira Castillo <mira.castillo@canopytax.com>
2022-01-17 01:58:17 -0700

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useMemo, useState } from 'react';
+
+const apiUrl = 'https://meta-weather.vercel.app/api'
+const defaultCities = ['725746', '44418', '2423945']
 
 function App() {
+  const [defaultCityWeather] = useMemo(() => {
+    let allWeather = []
+    defaultCities.forEach(city => {
+      async function buildWeatherInfo(){
+        let weather = await fetch(`${apiUrl}/location/${city}`)
+        allWeather = [...allWeather, weather.data]
+      }
+      buildWeatherInfo()
+    })
+    return allWeather
+  }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {defaultCityWeather?.map(data => (
+        <div>
+          <h1>{data.title}, {data.parent.title}</h1>
+          {data.consolidated_weather.map(day => (
+            <div>
+              <div>
+                <p>{day.applicable_date} {data.timezone_name}</p>
+              </div>
+              <div>
+                <div>
+                  <p>Predictability</p>
+                  <p>{day.predictability}</p>
+                </div>
+                <p>image</p>
+                <p>{day.weather_state_name}</p>
+                <p>{day.the_temp} C / F</p>
+              </div>
+              <div>
+                <p>Min: {day.min_temp}°C / °F</p>
+                <p>Max: {day.max_temp}°C / °F</p>
+                <p>Wind: {day.wind_direction_compass}. {day.wind_speed} mph</p>
+                <p>Visibility: {day.visibility} mi</p>
+                <p>Humidity: {day.humidity}%</p>
+                <p>Air pressure: {day.air_pressure}</p>
+              </div>
+            </div>
+          ))}
+        </div>
+      ))}
     </div>
   );
 }
